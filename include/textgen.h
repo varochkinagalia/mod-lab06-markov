1f8e3186d4464c07abf364a5996f2011cb4cb024
Варочкина Галя <147079170+varochkinagalia@users.noreply.github.com>
2024-04-23 20:12:31 +0300

    textgen.h

--- include/textgen.h
+++ include/textgen.h
@@ -1,29 +1,27 @@
+// Copyright 2022 UNN-IASR
 #pragma once
-#include <iostream>
-#include <fstream>
 #include <map>
 #include <vector>
 #include <string>
 #include <deque>
 #include <locale.h>
+#include <iostream>
+#include <fstream>
 typedef std::deque<std::string> prefix;
-class generator
-{
-private:
-	std::vector<std::string> words;
-public:
-    //чтение файла
-	std::vector<std::string> read_vchodnoi_fail(std::string filename);
-	
-	//создание таблицы
-	std::map<prefix, std::vector<std::string>> statetab;
-	int NPREF = 2;
-	std::map<prefix, std::vector<std::string>> sozdaem_tablicu(std::vector<std::string>words);
-
-	//генерация выходного текста
-	int MAXGEN = 1000;
-	std::deque<std::string> prefixi(std::map<prefix, std::vector<std::string>> statetab);
-	std::string suffix_poick(prefix pr, std::map<prefix, std::vector<std::string>> statetab);
-	std::string itogovii_text(std::map<prefix, std::vector<std::string>> statetab);
-	generator();
+class generator {
+ private:
+     std::vector<std::string> words;
+ public:
+     //чтение файла
+     std::vector<std::string> read_vchodnoi_fail(std::string filename);
+     //создание таблицы
+     std::map<prefix, std::vector<std::string>> statetab;
+     int NPREF = 2;
+     std::map<prefix, std::vector<std::string>> sozdaem_tablicu(std::vector<std::string>words);
+     //генерация выходного текста
+     int MAXGEN = 1000;
+     std::deque<std::string> prefixi(std::map<prefix, std::vector<std::string>> statetab);
+     std::string suffix_poick(prefix pr, std::map<prefix, std::vector<std::string>> statetab);
+     std::string itogovii_text(std::map<prefix, std::vector<std::string>> statetab);
+     generator();
 };
