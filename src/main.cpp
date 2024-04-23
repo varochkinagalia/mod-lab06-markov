5c6eead4b202eaec8f38b69f84db3b181fde6f6a
Варочкина Галя <147079170+varochkinagalia@users.noreply.github.com>
2024-04-23 21:26:38 +0300

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
-#include <map>
-#include <vector>
-#include <string>
-#include <deque>
-#include <locale.h>
+// Copyright 2022 UNN-IASR
 #include "textgen.h"
 #include <iostream>
 #include <ostream>
 #include <fstream>
+#include <locale.h>
+#include <map>
+#include <vector>
+#include <string>
+#include <deque>
 typedef std::deque<std::string> prefix;
 using namespace std;
 int main() {
@@ -14,6 +15,6 @@ int main() {
   string file = "skazka.txt";
   generator gen;
   vector<string>words = gen.read_vchodnoi_fail(file);
-  map<prefix, vector<string>>tablica = gen.sozdaem_tablicu(words);
+  map<prefix, vector<string>> tablica = gen.sozdaem_tablicu(words);
   string itog = gen.itogovii_text(tablica);
 }
