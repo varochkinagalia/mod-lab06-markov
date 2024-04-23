084d47e68f6fed5762df72accd17fad9e0c38ac7
Варочкина Галя <147079170+varochkinagalia@users.noreply.github.com>
2024-04-23 23:11:36 +0300

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 typedef std::deque<std::string> prefix;
 int main() {
   setlocale(LC_ALL, "ru");
-  string file = "skazka.txt";
+  std::string file = "skazka.txt";
   generator gen;
   std::vector<std::string>words = gen.read_vchodnoi_fail(file);
   std::map<prefix, std::vector<std::string>> tablica =
